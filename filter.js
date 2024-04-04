b3ff7f78f7c173cc2231cfbba1b4b8d538e49b74
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-04-05 03:39:57 +0600

    find added

--- filter.js
+++ filter.js
@@ -1,5 +1,4 @@
 // filter selects elements based on a condition and return an array with the elements that fulfilled the condition.
-const numbers = [1, 4, 5, 6, 8, 9];
 
 const playersInchHeight = [75, 65, 67, 72, 55, 59];
 const selected = playersInchHeight.filter(p => p % 2 === 0);
---
+++ find.js
@@ -0,0 +1,3 @@
+const playersInchHeight = [75, 65, 67, 72, 55, 59];
+const selected = playersInchHeight.find(player => player > 70);
+console.log(selected);
