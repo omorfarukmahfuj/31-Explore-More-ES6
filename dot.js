acd8e287c2f26cff72e84109be9ceb45b7008499
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-04-05 19:03:34 +0600

    dot-notationd updated

--- dot.js
+++ dot.js
@@ -1,8 +1,16 @@
 const person = {
   name: 'Omor',
-  job: 'Web Developer'
+  job: 'Web Developer',
+  3: 'third',
+  'key-word': true
+
 }
 const prop = 'job';
 console.log(person.job);
 console.log(person['job']);
 console.log(person[prop]);
+
+
+// console.log(person.3); this will give an error
+console.log(person['3']);
+console.log(person["key-word"]);
