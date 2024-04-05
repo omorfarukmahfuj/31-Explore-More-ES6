6f80146cea62dd9e22a38bd5cc3975e60c1e444b
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-04-05 18:59:43 +0600

    dot notation added

---
+++ dot.js
@@ -0,0 +1,8 @@
+const person = {
+  name: 'Omor',
+  job: 'Web Developer'
+}
+const prop = 'job';
+console.log(person.job);
+console.log(person['job']);
+console.log(person[prop]);
--- this.js
+++ this.js
@@ -3,7 +3,17 @@ class Person {
     this.name = name;
     this.age = age;
   }
+
+  sleep() {
+    console.log(`Sleeping now ${this.name}`);
+  }
 }
 
 const person1 = new Person('Omor', 20);
 console.log(person1);
+person1.sleep();
+
+
+const person2 = new Person('Faruk', 20);
+console.log(person2);
+person2.sleep();
